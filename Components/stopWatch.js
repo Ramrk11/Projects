8718540adef9423e395f312130f0c3929eeb4f5f
Ramrk11 <39863783+Ramrk11@users.noreply.github.com>
2018-11-01 11:24:55 +0530

    Stopwatch js object

---
+++ Components/stopWatch.js
@@ -0,0 +1,36 @@
+function stopWatch() {
+	let startTime,
+		endTime,
+		running,
+		duration = 0;
+
+	this.start = function() {
+		if (running) throw new Error('stopwatch started already');
+
+		running = true;
+
+		startTime = new Date();
+	};
+	this.stop = function() {
+		if (!running) throw new Error('stopwatch stopped already');
+
+		running = false;
+
+		endTime = new Date();
+
+		const seconds = (startTime.getTime() - endTime.getTime()) / 1000;
+		duration += seconds;
+	};
+	this.reset = function() {
+		startTime = null;
+		endTime = null;
+		running = false;
+		duration = 0;
+	};
+
+	Object.defineProperty(this, 'duration', {
+		get: function() {
+			return duration;
+		},
+	});
+}
